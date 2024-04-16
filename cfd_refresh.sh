c417ff6d028c29079c0a45f1e275a2bce170d980
wsbh <9865509+wsbh@users.noreply.github.com>
2024-04-16 23:21:20 +0800

    SOCKS

--- cfd_refresh.sh
+++ cfd_refresh.sh
@@ -17,9 +17,11 @@ perform_variable_substitution ${VAR_NAMES[@]} 'ARGO_URL' < template_client_confi
 vmlink=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"${DISPLAY_NAME}vmess\",\"add\":\"$ARGO_URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$ARGO_URL\",\"path\":\"$VMESS_WSPATH?ed=2560\",\"tls\":\"tls\"}" | base64 -w 0)
 vllink=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$ARGO_URL":443?encryption=none&security=tls&type=ws&host="$ARGO_URL"&path="$VLESS_WSPATH"#${DISPLAY_NAME}vless"
 vllinks=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$ARGO_URL":443?encryption=none&security=tls&type=httpupgrade&host="$ARGO_URL"&path="$TROJAN_WSPATH"#${DISPLAY_NAME}vless"
-
+vmlink_socks=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"${DISPLAY_NAME}vmess\",\"add\":\"$ARGO_URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$ARGO_URL\",\"path\":\"$VMESS_WSPATH_SOCKS?ed=2560\",\"tls\":\"tls\"}" | base64 -w 0)
+vllink_socks=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$ARGO_URL":443?encryption=none&security=tls&type=ws&host="$ARGO_URL"&path="$VLESS_WSPATH_SOCKS"#${DISPLAY_NAME}vless"
+vllinks_socks=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$ARGO_URL":443?encryption=none&security=tls&type=httpupgrade&host="$ARGO_URL"&path="$TROJAN_WSPATH_SOCKS"#${DISPLAY_NAME}vless"
 
 # 产生订阅
-echo -e "$vmlink\n$vllink\n$vllinks" | base64 -w 0 > /usr/share/nginx/html/$UUID.txt
+echo -e "$vmlink\n$vllink\n$vllinks$vmlink_socks\n$vllink_socks\n$vllinks_socks" | base64 -w 0 > /usr/share/nginx/html/$UUID.txt
 
-perform_variable_substitution ${VAR_NAMES[@]} 'ARGO_URL' 'vmlink' 'vllink' 'vllinks' < template_webpage.html > "/usr/share/nginx/html/$UUID.html"
+perform_variable_substitution ${VAR_NAMES[@]} 'ARGO_URL' 'vmlink' 'vllink' 'vllinks' 'vmlink_socks' 'vllink_socks' 'vllinks_socks' < template_webpage.html > "/usr/share/nginx/html/$UUID.html"
